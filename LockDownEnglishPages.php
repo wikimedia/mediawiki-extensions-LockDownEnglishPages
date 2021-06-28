3a99ec64bfcb06364a325d016b1a026ac437c8ff
vladshapik <vlad.shapik@speedandfunction.com>
2021-06-28 19:21:51 +0300

    Avoid using User group methods
    Rewrite the extension to use new
    system of handling hooks. Replace
    User group methods with UserGroupManager
    methods.
    
    Bug: T281872
    Change-Id: I571f584997b92208a7bd1ad3adc9f8e082e9542d

--- LockDownEnglishPages.php
+++ LockDownEnglishPages.php
@@ -1,36 +1,55 @@
 <?php
 /**
  * LockDownEnglishPages -- prevents non-staff users from editing the English
- * interface messages on the Inteface Messages Wiki
+ * interface messages on the Interface Messages Wiki
  *
  * @file
  * @ingroup Extensions
- * @version 0.3.0
- * @date 30 July 2020
+ * @version 0.3.1
+ * @date 29 June 2021
  * @author Jack Phoenix
  * @license http://en.wikipedia.org/wiki/Public_domain Public domain
  */
 
-use MediaWiki\MediaWikiServices;
+use MediaWiki\Permissions\Hook\UserCanHook;
+use MediaWiki\Permissions\PermissionManager;
+use MediaWiki\User\UserGroupManager;
 
-class LockDownEnglishPages {
+class LockDownEnglishPages implements UserCanHook {
+	/**
+	 * @var UserGroupManager
+	 */
+	private $userGroupManager;
 
 	/**
-	 * @param Title &$title
-	 * @param User &$user
+	 * @var PermissionManager
+	 */
+	private $permissionManager;
+
+	/**
+	 * @param UserGroupManager $userGroupManager
+	 * @param PermissionManager $permissionManager
+	 */
+	public function __construct( UserGroupManager $userGroupManager, PermissionManager $permissionManager ) {
+		$this->userGroupManager = $userGroupManager;
+		$this->permissionManager = $permissionManager;
+	}
+
+	/**
+	 * @param Title $title
+	 * @param User $user
 	 * @param string $action
-	 * @param array|IApiMessage &$result
+	 * @param string &$result
 	 *
 	 * @return bool
 	 */
-	public static function onUserCan( &$title, &$user, $action, &$result ) {
-		$pm = MediaWikiServices::getInstance()->getPermissionManager();
+	public function onUserCan( $title, $user, $action, &$result ) {
 		// We want to prevent editing of MediaWiki pages for users who have the
 		// editinterface right but who are not staff when the action is 'edit'
 		if (
 			$title->getNamespace() == NS_MEDIAWIKI &&
-			$pm->userHasRight( $user, 'editinterface' ) &&
-			!in_array( 'staff', $user->getEffectiveGroups() ) &&
+			$this->permissionManager->userHasRight( $user, 'editinterface' ) &&
+			!in_array( 'staff', $this->userGroupManager->getUserEffectiveGroups( $user ) ) &&
 			$action == 'edit'
 		) {
 			$pageTitle = $title->getDBkey();
